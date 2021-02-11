8a3f65071fe30911ad69d7e8c1e30bfaeb2935e5
Eddie <eddie.ch.89d@gmail.com>
2021-02-11 19:03:02 +0100

    Exercises

--- src/main/java/se/lexicon/modelCarRegister/Car.java
+++ src/main/java/se/lexicon/modelCarRegister/Car.java
@@ -3,9 +3,20 @@ package se.lexicon.modelCarRegister;
 
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
 
 
 
 public class Car implements Serializable {
     //carId, regNumber, brand, model, regDate and owner
+    private UUID carId;
+    private String regNumber;
+    private String brand;
+    private String model;
+    private LocalDate regDate;
+    private String owner;
+
+    public Car() {
+    }
 }
